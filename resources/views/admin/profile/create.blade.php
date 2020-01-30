7d69045121ee90529a987008b2f1fae08d5f1348
EC2 Default User <ec2-user@ip-172-31-39-93.us-east-2.compute.internal>
2020-01-30 12:02:04 +0000

    kadai

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,11 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>課題クリエイト</title>
-    </head>
-    <body>
-        <h1>練習１</h1>
-    </body>
-</html>
+@extends('layouts.profile')
+@section('title', '課題１１作成')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>課題１１</h2>
+            </div>
+        </div>
+    </div>
+@endsection
