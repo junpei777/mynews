adcbef16ca00ebfda9ff1b253cbbe6c16a9a965a
EC2 Default User <ec2-user@ip-172-31-39-93.us-east-2.compute.internal>
2020-02-23 08:02:49 +0000

    課題１８

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ Route::group(['prefix' => 'admin'], function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'NewsController@index');
+Route::get('/profile', 'ProfileController@index');
